731f2fbf1b1ce17d9ed1f8f7787d36790dc5883f
Riku <rikudonoyamaguchi@gmail.com>
2023-02-07 16:42:33 +0900

    head情報の編集

--- login.php
+++ login.php
@@ -38,13 +38,14 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     }
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="ja">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ログインする</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 
 <body>
